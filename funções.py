3d10fd8e7e83ad7677f93b537c9226869da79d61
LuanaNorberto <luanadcruz.91@gmail.com>
2024-07-13 13:54:42 -0300

    Definição e chamada de uma função

--- funções.py
+++ funções.py
@@ -4,3 +4,6 @@ def soma(a,b):
     return a+b
 resultado = soma(5,10)
 print(resultado)
+
+# defini que a é 5 e b são 10.
+# pedi para retornar o resultado da soma de a e b, sendo assim, ele apresentou 15.

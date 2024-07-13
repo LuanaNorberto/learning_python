3ac6b4e3215619e80b87d329fa380ac7b34964bc
LuanaNorberto <luanadcruz.91@gmail.com>
2024-07-13 14:15:24 -0300

    Dicionários

--- dicionários.py
+++ dicionários.py
@@ -6,3 +6,6 @@ print(dicionario['nome'])
 # adicional
 
 print(dicionario['idade'])
+
+# criei um dicionário com informações de nome e idade. 
+# com o print, buusquei o que queria, nome e idade, respectivamente.

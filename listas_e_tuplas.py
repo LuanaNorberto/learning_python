3e8f6ffa8be6292f63158b02ee92b0b453851d6b
LuanaNorberto <luanadcruz.91@gmail.com>
2024-07-13 14:03:43 -0300

    Lista e tupla

---
+++ listas_e_tuplas.py
@@ -0,0 +1,8 @@
+# Lista e tupla
+
+lista = [1,2,3,4,5]
+tupla = (1,2,3,4,5)
+print(lista[0])
+print(tupla [0])
+
+# Não vi diferença entre as duas, exceto que ambas iniciam sua contagem pelo 0, neste caso, 0 é 1.

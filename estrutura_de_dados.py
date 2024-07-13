84587efd03df1eab0ff7ed2dbe816071a60d760f
LuanaNorberto <luanadcruz.91@gmail.com>
2024-07-13 14:19:40 -0300

    Conjuntos

---
+++ estrutura_de_dados.py
@@ -0,0 +1,11 @@
+# Conjuntos
+
+conjunto = {1,2,3,4,5}
+print(conjunto)
+
+# adicional
+
+conjunto = {'maça','pêra','limão','abacate'}
+print(conjunto)
+
+# criei uma "lista" ele imprimiu a informação na mesma ordem.

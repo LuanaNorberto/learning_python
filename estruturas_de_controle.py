b317d662d2fda0d24cdef19b3b6c166c50546ed2
LuanaNorberto <luanadcruz.91@gmail.com>
2024-07-13 13:36:56 -0300

    Exemplo de condicional e loop

--- estruturas_de_controle.py
+++ estruturas_de_controle.py
@@ -5,3 +5,6 @@ for i in range (5):
         print(f'{i} é par')
     else:
         print(f'{i} é impar')
+        
+        # range cria uma sequência de 0 a 5 (0,1,2,3,4)e verifica o que é par e ímpar. 
+        # a porcentagem é uma divisão, verifica se i = 0 é divisível por 2, neste caso,0 é ímpar.
